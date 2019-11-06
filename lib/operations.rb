a657412dfb24b2ccce6c092b18869607ae1a98b2
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-06 13:39:32 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
 def unsafe?(speed)
   if speed < 40 || speed > 60
-    p "true"
+    p true
   else
-    p "false"
+    p false
   end
 end
 
 def not_safe?(speed)
- speed < 40 || speed > 60 ? "true" : "false"
+ speed < 40 || speed > 60 ? true : false
 end
